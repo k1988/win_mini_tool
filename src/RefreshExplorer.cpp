78146e437c18f521bb63c48271389ef343f76df1
zhaohy <zhaohaiyang.1988@gmail.com>
2015-07-03 17:57:39 +0800

    问题修复：修正代码格式和注册表路径和值的多余的空格

--- src/RefreshExplorer.cpp
+++ src/RefreshExplorer.cpp
@@ -1,38 +1,37 @@
 #include <windows.h>
-#include <TCHAR.h>
 
 #pragma comment(lib, "User32.lib")
 #pragma comment(lib, "Advapi32.lib")
 
 void FreshExplorer()
 {
-	HKEY   hKey; 
-	struct   HKEY__*RootKey; 
-	TCHAR   *SubKey; 
-	DWORD   dwType=REG_SZ;	
-	DWORD   dwLength=256; 
-	TCHAR   *ValueName; 
-	LPBYTE   SetContent_S; 
-	RootKey=HKEY_CURRENT_USER;                                                                                 
-	SubKey= "Control   Panel\\Desktop\\WindowMetrics ";   
-	char   content[256]; 
-	ValueName= "Shell   Icon   Size "; 
+	HKEY hKey; 
+	struct HKEY__*RootKey; 
+	char *SubKey; 
+	DWORD dwType=REG_SZ;	
+	DWORD dwLength=256; 
+	char *ValueName; 
+	LPBYTE SetContent_S; 
+	RootKey=HKEY_CURRENT_USER; 
+	SubKey= "Control Panel\\Desktop\\WindowMetrics"; 
+	char content[256]; 
+	ValueName= "Shell Icon Size"; 
 	RegOpenKeyEx(RootKey,SubKey,0,KEY_READ,&hKey); 
-	RegQueryValueEx(hKey,ValueName,NULL,&dwType,(unsigned   char   *)content,&dwLength); 
+	RegQueryValueEx(hKey,ValueName,NULL,&dwType,(unsigned char  *)content,&dwLength); 
 	RegCloseKey(hKey); 
 	RootKey=HKEY_CURRENT_USER; 
-	SubKey= "Control   Panel\\Desktop\\WindowMetrics "; 
-	ValueName= "Shell   Icon   Size "; 
-	SetContent_S=LPBYTE( "24 "); 
+	SubKey= "Control Panel\\Desktop\\WindowMetrics "; 
+	ValueName= "Shell Icon Size"; 
+	SetContent_S=LPBYTE("24"); 
 	RegOpenKeyEx(RootKey,SubKey,0,KEY_WRITE,&hKey); 
-	RegSetValueEx(hKey,ValueName,NULL,REG_SZ,SetContent_S, _tcslen((TCHAR*)SetContent_S)); 
+	RegSetValueEx(hKey,ValueName,NULL,REG_SZ,SetContent_S, strlen((char*)SetContent_S)); 
 	RegCloseKey(hKey); 
-	SendMessageTimeout(HWND_BROADCAST,WM_SETTINGCHANGE,   SPI_SETNONCLIENTMETRICS,0,SMTO_ABORTIFHUNG,5000,NULL); 
+	SendMessageTimeout(HWND_BROADCAST,WM_SETTINGCHANGE, SPI_SETNONCLIENTMETRICS,0,SMTO_ABORTIFHUNG,5000,NULL); 
 	SetContent_S=LPBYTE(content); 
 	RegOpenKeyEx(RootKey,SubKey,0,KEY_WRITE,&hKey); 
-	RegSetValueEx(hKey,ValueName,NULL,REG_SZ,SetContent_S,_tcslen((TCHAR*)SetContent_S)); 
+	RegSetValueEx(hKey,ValueName,NULL,REG_SZ,SetContent_S,strlen((char*)SetContent_S)); 
 	RegCloseKey(hKey); 
-	SendMessageTimeout(HWND_BROADCAST,WM_SETTINGCHANGE,   SPI_SETNONCLIENTMETRICS,0,SMTO_ABORTIFHUNG,5000,NULL);
+	SendMessageTimeout(HWND_BROADCAST,WM_SETTINGCHANGE, SPI_SETNONCLIENTMETRICS,0,SMTO_ABORTIFHUNG,5000,NULL);
 }
 void main()
 {
