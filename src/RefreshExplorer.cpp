396a3dfe0f0aa57b970e817026fdb8a9e8044a2e
zhaohy <zhaohaiyang.1988@gmail.com>
2015-07-03 17:51:34 +0800

    功能开发：添加刷新桌面图标的工具

---
+++ src/RefreshExplorer.cpp
@@ -0,0 +1,40 @@
+#include <windows.h>
+#include <TCHAR.h>
+
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Advapi32.lib")
+
+void FreshExplorer()
+{
+	HKEY   hKey; 
+	struct   HKEY__*RootKey; 
+	TCHAR   *SubKey; 
+	DWORD   dwType=REG_SZ;	
+	DWORD   dwLength=256; 
+	TCHAR   *ValueName; 
+	LPBYTE   SetContent_S; 
+	RootKey=HKEY_CURRENT_USER;                                                                                 
+	SubKey= "Control   Panel\\Desktop\\WindowMetrics ";   
+	char   content[256]; 
+	ValueName= "Shell   Icon   Size "; 
+	RegOpenKeyEx(RootKey,SubKey,0,KEY_READ,&hKey); 
+	RegQueryValueEx(hKey,ValueName,NULL,&dwType,(unsigned   char   *)content,&dwLength); 
+	RegCloseKey(hKey); 
+	RootKey=HKEY_CURRENT_USER; 
+	SubKey= "Control   Panel\\Desktop\\WindowMetrics "; 
+	ValueName= "Shell   Icon   Size "; 
+	SetContent_S=LPBYTE( "24 "); 
+	RegOpenKeyEx(RootKey,SubKey,0,KEY_WRITE,&hKey); 
+	RegSetValueEx(hKey,ValueName,NULL,REG_SZ,SetContent_S, _tcslen((TCHAR*)SetContent_S)); 
+	RegCloseKey(hKey); 
+	SendMessageTimeout(HWND_BROADCAST,WM_SETTINGCHANGE,   SPI_SETNONCLIENTMETRICS,0,SMTO_ABORTIFHUNG,5000,NULL); 
+	SetContent_S=LPBYTE(content); 
+	RegOpenKeyEx(RootKey,SubKey,0,KEY_WRITE,&hKey); 
+	RegSetValueEx(hKey,ValueName,NULL,REG_SZ,SetContent_S,_tcslen((TCHAR*)SetContent_S)); 
+	RegCloseKey(hKey); 
+	SendMessageTimeout(HWND_BROADCAST,WM_SETTINGCHANGE,   SPI_SETNONCLIENTMETRICS,0,SMTO_ABORTIFHUNG,5000,NULL);
+}
+void main()
+{
+	FreshExplorer();
+}
