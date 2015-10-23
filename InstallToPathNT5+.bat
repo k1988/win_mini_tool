04907433bd98ebade16fff5a8f6f32d4b25d6c6f
zhaohy <zhaohaiyang.1988@gmail.com>
2015-10-23 16:38:13 +0800

    问题修复：修复win10上安装脚本的错误(reg返回格式发生变化）

---
+++ InstallToPathNT5+.bat
@@ -0,0 +1,20 @@
+@echo off
+@cd %~dp0%
+@%~d0%
+@set regpath="HKEY_CURRENT_USER\Environment"
+
+for,/f,"skip=2 tokens=1,2,*",%%a,in,('reg query %regpath% /v Path'),do,(
+        echo 原环境变量:
+        :echo %%a :path
+        :echo %%b :注册表类型
+        echo %%c
+	echo;
+	:FIXME TODO 添加判断当前path是否已经有了当前目录
+	echo 更改后环境变量
+	echo %%c;%CD%;%CD%\bin;%CD%\bats;%CD%\scripts
+        reg add %regpath% /v Path /t REG_EXPAND_SZ /d "%%c;%CD%;%CD%\bin;%CD%\bats;%CD%\scripts;" /f
+	goto :eof
+)
+
+:eof
+pause
