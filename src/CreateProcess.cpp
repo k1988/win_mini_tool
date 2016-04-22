f2df8dde2478c6a40c9f4bdea26f0f99020df3f8
zhaohy <zhaohaiyang.1988@gmail.com>
2016-04-22 09:39:23 +0800

    功能开发：添加一创建进程的程序，将其命令行再执行，用来测试一些会检测父进程的程序

---
+++ src/CreateProcess.cpp
@@ -0,0 +1,40 @@
+#include <windows.h>
+#include <stdio.h>
+
+void main(char argc,char* argv[])
+{
+    if (argc == 1)
+    {
+        printf("Usage: CreateProcess <CMD>");
+    }
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    // Start the child process. 
+    if( !CreateProcess( NULL,   // No module name (use command line). 
+        argv[1], // Command line. 
+        NULL,             // Process handle not inheritable. 
+        NULL,             // Thread handle not inheritable. 
+        FALSE,            // Set handle inheritance to FALSE. 
+        0,                // No creation flags. 
+        NULL,             // Use parent's environment block. 
+        NULL,             // Use parent's starting directory. 
+        &si,              // Pointer to STARTUPINFO structure.
+        &pi )             // Pointer to PROCESS_INFORMATION structure.
+    ) 
+    {
+        printf( "CreateProcess failed (%d).\n", GetLastError() );
+        return;
+    }
+
+    // Wait until child process exits.
+    WaitForSingleObject( pi.hProcess, INFINITE );
+
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+}
