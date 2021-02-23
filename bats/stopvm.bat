ad2d2d56fc7fd3ddd9e55b5ff82ad05ac3d973d2
k1988 <zhaohaiyang_1988@163.com>
2021-02-23 18:21:25 +0800

    功能改进：优化stop vm脚本，增加几个新服务名称

--- bats/stopvm.bat
+++ bats/stopvm.bat
@@ -1,3 +1,9 @@
+sc stop "VMwareHostd"
+sc config "VMwareHostd" start= demand
+
+sc stop "VMUSBArbService"
+sc config "VMUSBArbService" start= demand
+
 sc stop "VMware NAT Service"
 sc config "VMware NAT Service" start= demand
 
@@ -10,6 +16,10 @@ sc config "VMAuthdService" start= demand
 sc stop "VMUSBArbService"
 sc config "VMUSBArbService" start= demand
 
-sc stop "VMAuthdService"
-sc config "VMAuthdService" start= demand
+sc stop "vmms"
+sc config "vmms" start= demand
+
+sc stop "vmcompute"
+sc config "vmcompute" start= demand
+
 pause
